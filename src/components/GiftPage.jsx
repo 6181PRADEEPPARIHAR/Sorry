eb2a4aaf117be0fb9e3efd40b79cc97a8d80b660
6181PRADEEPPARIHAR <145856469+6181PRADEEPPARIHAR@users.noreply.github.com>
2026-07-03 01:17:48 +0530

    Changes

--- src/components/GiftPage.jsx
+++ src/components/GiftPage.jsx
@@ -177,7 +177,7 @@ export default function GiftPage() {
                     delay: 1,
                   }}
                 >
-                  I Love You jii
+                  🧿
                 </motion.div>
               </motion.div>
 
@@ -203,8 +203,7 @@ export default function GiftPage() {
                       transition={{ duration: 0.8, delay: 0.6 }}
                       className="text-lg text-purple-200 leading-relaxed"
                     >
-                      Just like my love for you. I promise to cherish you every single day, and never take your
-                      beautiful heart for granted again.💖
+                      Just like that my Friendship and My Love for you will never wilt.I am always there for you and with you jii. I promise to cherish you every single day, and never take your beautiful heart for granted again.💖
                     </motion.p>
                     <motion.p
                       initial={{ opacity: 0, y: 15 }}
